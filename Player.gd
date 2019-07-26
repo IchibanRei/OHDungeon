7c9ca4dc8ba345be0bb3e2801d8507e4d5262473
Rey <ichibanrei@gmail.com>
2019-07-25 22:13:34 -0700

    added art and basic player movement.

---
+++ Player.gd
@@ -0,0 +1,22 @@
+extends KinematicBody2D
+
+const SPEED = 250
+var motion = Vector2(0, 0)
+
+func _process(delta):
+	move()
+	move_and_slide(motion)
+
+
+func move():
+	if Input.is_action_pressed("Left") and not Input.is_action_pressed("Right"):
+		motion.x = -SPEED
+	elif Input.is_action_pressed("Right")and not Input.is_action_pressed("Left"):
+		motion.x = SPEED
+	elif Input.is_action_pressed("Up") and not Input.is_action_pressed("Down"):
+		motion.y = -SPEED
+	elif Input.is_action_pressed("Down") and not Input.is_action_pressed("Up"):
+		motion.y = SPEED
+	else:
+		motion.x = 0
+		motion.y = 0
